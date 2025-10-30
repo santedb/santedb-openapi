86581c009894af2ff1f29d7b1219e0c65945b9b4
Justin Fyfe <justin@fyfesoftware.ca>
2025-10-30 16:57:34 -0400

    Security fixes to wrapped session data

--- SanteDB.Messaging.OpenAPI/Rest/MetadataServiceBehavior.cs
+++ SanteDB.Messaging.OpenAPI/Rest/MetadataServiceBehavior.cs
@@ -107,7 +107,7 @@ namespace SanteDB.Messaging.Metadata.Rest
                 if (string.IsNullOrWhiteSpace(content))
                 {
                     var requestUrl = RestOperationContext.Current.IncomingRequest.Url;
-                    RestOperationContext.Current.OutgoingResponse.Redirect($"{requestUrl.Scheme}://{requestUrl.Host}:{requestUrl.Port}{requestUrl.AbsolutePath}/index.html");
+                    RestOperationContext.Current.OutgoingResponse.Redirect($"/index.html");
                     return new MemoryStream();
                 }
 
