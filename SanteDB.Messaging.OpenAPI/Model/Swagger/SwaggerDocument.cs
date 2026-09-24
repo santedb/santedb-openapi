328b8ffeb1eddc31880ee0debd14efa58d7dbf3a
Justin Fyfe <justin@fyfesoftware.ca>
2026-09-24 17:07:57 -0400

    Removed broken application authentication scheme

--- SanteDB.Messaging.OpenAPI/Model/Swagger/SwaggerDocument.cs
+++ SanteDB.Messaging.OpenAPI/Model/Swagger/SwaggerDocument.cs
@@ -106,18 +106,9 @@ namespace SanteDB.Messaging.Metadata.Model.Swagger
 
                     this.SecurityDefinitions = new Dictionary<string, SwaggerSecurityDefinition>()
                     {
-                        {  "svc_auth_password", new SwaggerSecurityDefinition()
+                        {  "svc_auth", new SwaggerSecurityDefinition()
                             {
                                 Flow = SwaggerSecurityFlow.password,
-                                Scopes = ApplicationServiceContext.Current.GetService<IRepositoryService<SecurityPolicy>>()?.Find(o=>o.ObsoletionTime == null).ToDictionary(o=>o.Oid, o=>o.Name),
-                                TokenUrl = $"{tokenUrl.ToString().Replace("0.0.0.0", RestOperationContext.Current.IncomingRequest.Url.Host)}/oauth2_token",
-                                Type = SwaggerSecurityType.oauth2
-                            }
-                        },
-                        {
-                            "svc_auth_client", new SwaggerSecurityDefinition()
-                            {
-                                Flow = SwaggerSecurityFlow.application,
                                 Scopes = ApplicationServiceContext.Current.GetService<IRepositoryService<SecurityPolicy>>()?.Find(o=>o.ObsoletionTime == null).ToDictionary(o=>o.Oid, o=>o.Name),
                                 TokenUrl = $"{tokenUrl.ToString().Replace("0.0.0.0", RestOperationContext.Current.IncomingRequest.Url.Host)}/oauth2_token",
                                 Type = SwaggerSecurityType.oauth2
